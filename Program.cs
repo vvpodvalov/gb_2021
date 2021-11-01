2a6d0544ef16e726744d7815636487a66fbf3ca8
vvpodvalov <92054360+vvpodvalov@users.noreply.github.com>
2021-11-01 19:43:18 +0300

    lesson1- answer

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как к Вам обращаться? ");
+            string name = Console.ReadLine();
+            string data = DateTime.Now.ToString(); //Присвоение даты
+            Console.WriteLine($"Привет, {name}, сегодня {data}"); //Вывод приветствия с датой
+        }
+    }
+}
