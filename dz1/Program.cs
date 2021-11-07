b5a44f2c6fa43ca33dde15cef97c0e6dfeba50db
vvpodvalov <vpodvalov@yandex.ru>
2021-11-07 15:22:10 +0300

    dz lesson_2

---
+++ dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите цифрами текущий месяц: ");
+             mn = Console.ReadLine();
+                       Console.WriteLine($"Cегодня" + mn.ToString("Y")); //Вывод приветствия с датой
+        }
+    }
+}
---
+++ dz2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dz2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную температуру в этом месяце:");
+            int minTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру в этом месяце:");
+            int maxTemp = Convert.ToInt32(Console.ReadLine()); ;
+            int srTemp = (minTemp + maxTemp) / 2;
+            Console.WriteLine($"Средняя температура в этом месяце" +""+srTemp);
+        }
+    }
+}
