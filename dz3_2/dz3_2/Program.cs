e71222c34aa39ec846992a00687fdb5141be7404
vpodvalov <vpodvalov@yandex.ru>
2021-11-09 18:10:12 +0300

    dz lesson2 and 3

---
+++ dz3_2/dz3_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dz3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // объявляем двумерный массив
+            int[,] myarray=new int[5,2]; 
+        }
+    }
+}
---
+++ dz3_3/dz3_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dz3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+
+            for (int i = str.Length-1; i >=0 ; i--) // пересчет от последнего элемента
+            {
+                Console.WriteLine(str[i]); // построчный вывод каждого символа строки
+            }
+
+        }
+    }
+}
