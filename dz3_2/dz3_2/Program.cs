2dc77403465ffa9d6fe9106605c5c42c666156a8
vpodvalov <vpodvalov@yandex.ru>
2021-11-09 18:52:34 +0300

    modify dz3_2

--- dz3_2/dz3_2/Program.cs
+++ dz3_2/dz3_2/Program.cs
@@ -7,7 +7,28 @@ namespace dz3_2
         static void Main(string[] args)
         {
             // объявляем двумерный массив
-            int[,] myarray=new int[5,2]; 
+            Console.WriteLine("Введите размер массива");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            string[,] myarray=new string[x,y];
+            Console.WriteLine("Введите данные в справочник");
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    myarray[i, j] = Console.ReadLine();
+                }
+            }
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.WriteLine(myarray[i, j] + "\t");
+                }
+            }
+            
+            
+            
         }
     }
 }
